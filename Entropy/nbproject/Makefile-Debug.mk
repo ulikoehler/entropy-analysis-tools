ef3c621b848cf3e86953354ecd1ab1a847d51669
Uli Koehler <ulikoehler@online.de>
2008-11-16 14:55:35 +0000

    Changed IDE to NetBeans C++

---
+++ Entropy/nbproject/Makefile-Debug.mk
@@ -0,0 +1,68 @@
+#
+# Gererated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=gfortran
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=build/Debug/GNU-Linux-x86
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/entropy.o \
+	${OBJECTDIR}/benchmarks/count1bitsbenchmark.o
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=-m64
+CXXFLAGS=-m64
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS} dist/Debug/GNU-Linux-x86/entropy
+
+dist/Debug/GNU-Linux-x86/entropy: ${OBJECTFILES}
+	${MKDIR} -p dist/Debug/GNU-Linux-x86
+	${LINK.cc} -lboost_program_options-gcc43-mt -o dist/Debug/GNU-Linux-x86/entropy ${OBJECTFILES} ${LDLIBSOPTIONS} 
+
+${OBJECTDIR}/entropy.o: entropy.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	$(COMPILE.cc) -g -o ${OBJECTDIR}/entropy.o entropy.cpp
+
+${OBJECTDIR}/benchmarks/count1bitsbenchmark.o: benchmarks/count1bitsbenchmark.cpp 
+	${MKDIR} -p ${OBJECTDIR}/benchmarks
+	$(COMPILE.cc) -g -o ${OBJECTDIR}/benchmarks/count1bitsbenchmark.o benchmarks/count1bitsbenchmark.cpp
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf:
+	${RM} -r build/Debug
+	${RM} dist/Debug/GNU-Linux-x86/entropy
+
+# Subprojects
+.clean-subprojects:
---
+++ Entropy/nbproject/Makefile-Release.mk
@@ -0,0 +1,63 @@
+#
+# Gererated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=gfortran
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=build/Release/GNU-Linux-x86
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/entropy.o
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=-m64 -fexpensive-optimizations -fomit-frame-pointer -march=core2 -ftree-vectorize -msse2 -msse3 -msse4 -ffunction-sections
+CXXFLAGS=-m64 -fexpensive-optimizations -fomit-frame-pointer -march=core2 -ftree-vectorize -msse2 -msse3 -msse4 -ffunction-sections
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS} dist/Release/GNU-Linux-x86/entropy
+
+dist/Release/GNU-Linux-x86/entropy: ${OBJECTFILES}
+	${MKDIR} -p dist/Release/GNU-Linux-x86
+	${LINK.cc} -lboost_program_options-gcc43-mt -o dist/Release/GNU-Linux-x86/entropy -s ${OBJECTFILES} ${LDLIBSOPTIONS} 
+
+${OBJECTDIR}/entropy.o: entropy.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	$(COMPILE.cc) -O3 -Wall -s -o ${OBJECTDIR}/entropy.o entropy.cpp
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf:
+	${RM} -r build/Release
+	${RM} dist/Release/GNU-Linux-x86/entropy
+
+# Subprojects
+.clean-subprojects:
