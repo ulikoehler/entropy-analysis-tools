e1c5d5e7412d4e9d5a9928a18a25b6a24ac49c99
Uli Koehler <ulikoehler@online.de>
2008-12-14 12:38:41 +0100

    Entropy: Added some more r-scripts and versions of existing r-scripts for plotting into png files instead of svg files

---
+++ Entropy/r-scripts/2d-scatterplot.png.r
@@ -0,0 +1,12 @@
+#You need the cairo package from CRAN installed.
+#If this is not possible, change svg("stats.svg") e.g. to jpeg("stats.jpg")
+#Use with a 2-column stats.dat
+d <- read.csv(file="stats.dat", header=TRUE,sep=",")
+png("2d-scatterplot.png",width=2500,height=4000)
+plot(x=d[,1],
+y=d[,2],
+type="p",
+col=heat.colors(length(d[,1])),
+main="Binary file analysis results")
+dev.off()
+q()
