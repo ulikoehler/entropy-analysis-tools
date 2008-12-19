5c6d033dc2188490321e055ecf5f7f548f18ff4c
Uli Koehler <ulikoehler@online.de>
2008-12-19 22:31:55 +0100

    RandGen: Fixed some minor bugs in counter.py. Script main workflow (randgen->chprec->count) works now.

--- RandGen/scripts/counter.py
+++ RandGen/scripts/counter.py
@@ -3,8 +3,12 @@ from __future__ import with_statement
 from optparse import OptionParser
 
 parser = OptionParser()
-parser.add_option("-o", "--out", dest="output",default="out.txt", help="Statistics output file")
-parser.add_option("-i","--in",dest="input",default="test.txt", help="Data input file")
+parser.add_option("-i","--in",dest="input",help="Data input file")
+parser.add_option("-o", "--out", dest="output",help="Statistics output file")
+parser.add_option("-s","--separator",dest="separator",help="CSV separator")
+#Set the defaults
+parser.set_defaults(output="out.txt",separator=",")
+#Do the actual parsing
 (options,args) = parser.parse_args()
 
 #occurrences dictionary
@@ -20,6 +24,6 @@ with open(options.input) as fin:
 		occ[line] += 1L
 	
 #write the data
-with open(options.output) as fin:
-	for k,n in occ:
-		print >> fout, k , n
+with open(options.output, "w") as fout:
+	for k in occ:
+		fout.write(k + options.separator + str(occ[k]) + "\n")
