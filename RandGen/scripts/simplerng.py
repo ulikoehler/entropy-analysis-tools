42702ce585438bdbbbfb4b2039d7fc2cad473284
Uli Koehler <ulikoehler@online.de>
2008-12-19 18:04:34 +0100

    RandGen: (simplerng.py) Corrected all syntax errors. Runs but main functionality has not yet been tested.

--- RandGen/scripts/simplerng.py
+++ RandGen/scripts/simplerng.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 from __future__ import with_statement
 from random import *
-from sys import *
+import sys
 
-outfileName = argv[1]
-count = (long) argv[2]
+if len(sys.argv) < 2: #Output file and count needed
+	print "You have to supply at least 2 arguments"
+	sys.exit(1)
+
+outfileName = sys.argv[1]
+count = int(sys.argv[2])
 
 #Parse the options
 try:
@@ -15,25 +19,25 @@ except getop.GetoptError, err:
 	sys.exit(2)
 	
 for o, a in opts:
-	if o in ("-h", "--help")
+	if o in ("-h", "--help"):
 		usage()
 		sys.exit(2)
-	elif o in ("-d", "--distribution")
+	elif o in ("-d", "--distribution"):
 		distribution = a
-	elif o in ("-p", "--param1")
-		param1 = (double) a
-	elif o in("-q", "--param2")
-		param2 = (double) a
-	elif o in("-l", "--lower")
-		lower = (int) a
-	elif o in("-q", "--upper")
-		upper = (int) a
+	elif o in ("-p", "--param1"):
+		param1 = double(a)
+	elif o in("-q", "--param2"):
+		param2 = double(a)
+	elif o in("-l", "--lower"):
+		lower = int(a)
+	elif o in("-q", "--upper"):
+		upper = int(a)
 		
 #
 #Set the right random generator function pointer
 #
 fgen = lambda: random() #Default
-elif distribution == "uniform":
+if distribution == "uniform":
 	fgen = lambda: uniform(lower,upper)
 elif distribution == "beta":
 	fgen = lambda: betavariate(param1,param2)
@@ -56,5 +60,5 @@ elif distribution == "weibullvariate":
 	
 #Main loop
 with open(outfileName, "w") as outfile:
-	for i in xrange(count)
+	for i in xrange(count):
 		print >> outfile, fgen()
