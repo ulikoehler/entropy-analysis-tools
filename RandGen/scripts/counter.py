b97752aa59bc90ddec9e644cb75fe415c708b32b
Uli Koehler <ulikoehler@online.de>
2008-12-19 20:40:11 +0100

    RandGen: Added counter.py to count occurrences of a number in a file (from old code, untested yet).

---
+++ RandGen/scripts/counter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from __future__ import with_statement
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("-o", "--out", dest="output",default="out.txt", help="Statistics output file")
+parser.add_option("-i","--in",dest="input",default="test.txt", help="Data input file")
+(options,args) = parser.parse_args()
+
+#occurrences dictionary
+occ = {}
+
+#read the data
+with open(options.input) as fin:
+	for line in fin:
+		line = line.strip()
+		if not line in  occ:
+			occ[line] = 1L
+			continue
+		occ[line] += 1L
+	
+#write the data
+with open(options.output) as fin:
+	for k,n in occ:
+		print >> fout, k , n
