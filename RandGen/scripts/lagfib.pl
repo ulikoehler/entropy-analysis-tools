80dc4442a8c85e31b4d78af56ce5dd0773000075
Uli Koehler <ulikoehler@online.de>
2008-12-27 23:32:43 +0100

    RandGen: lagfib.pl: Added "real" command line interface, seems to work; added some comments.

--- RandGen/scripts/lagfib.pl
+++ RandGen/scripts/lagfib.pl
@@ -8,42 +8,30 @@
 #m = The modulus (as above)
 #A,B as above
 #[seed list] = A comma-separated list of seed values, at least A and at least B elements
-#
-#TODO:
-#The command line syntax as above is not yet implemented so you are asked for
-#the values via stdin
 
-#Ask for the values
-print "Output file:";
-	my $ofname = <>;
-	chomp $ofname;
-print "Numbers to generate:";
-	my $n = <>;
-	chomp $n;
-print "Modulus:";
-	my $m = <>;
-	chomp $m;
-print "A:";
-	my $a = <>;
-	chomp $a;
-print "B:";
-	my $b = <>;
-	chomp $b;
-print "Seeds (comma separated):";
-	$sraw = <>;
-	chomp $sraw;
-	my @s = split(/,/,$sraw);
+#Parse the command line
+my $ofname = shift;
+my $n = shift;
+my $m = shift;
+my $a = shift;
+my $b = shift;
+my $sraw = shift;
+my @s = split(/,/,$sraw);
 
+#Open the output file
 open(OUTFILE, ">" . $ofname) or die "Can't open output file!";
+#Main loop
 while($n > 0)
 {
+	#Generate a new value
 	$y = ($s[$a-1] + $s[$b-1]) % $m; #-1: First array index is 0
 	print OUTFILE $y . "\n";
 	#Add the current value to the beginning of the array and
 	#remove the last value to save memory.
 	unshift(@s,$y);
 	pop(@s);
-	
-	$n--; #Decrement the loop counter
+	#Decrement the loop counter
+	$n--;
 }
+#Close the output file
 close OUTFILE;
