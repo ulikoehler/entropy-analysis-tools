af8a47c57a3fc7d595223b8705e771b683bd1ac8
Uli Koehler <ulikoehler@online.de>
2008-12-28 19:32:15 +0100

    Entropy: bitpixel.pl: Added command-line options for output file and width, added usage and description header.

--- Entropy/scripts/bitpixel.pl
+++ Entropy/scripts/bitpixel.pl
@@ -1,7 +1,23 @@
 #!/usr/bin/perl -w
+#bitpixel.pl
+#This script is intended to display binary patterns in files having one integer per line.
+#If a bit is set, the appropriate pixel is set black, otherwise white.
+#The syntax is:
+#bitpixel.pl [infilename] [outfilename] [width]
+#Where:
+#infilename = The file to read the numbers from, one integer per line-
+#outfilename = The PNG file to write the resulting image to. Defaults to out.png.
+#width = The width in bits to analyze. Defaults to 32.
+#libcairo-perl with PNG backend support is needed
 use Cairo;
 
+#Check library requirements
+die "Cairo PNG backend is not supported!\n" unless (Cairo::HAS_PNG_FUNCTIONS);
+
 my $infilename = shift;
+my $outfilename = shift || "out.png";
+my $imgWidth = shift || 32;
+
 #Read the data from the input file
 open(INFILE, "<" . $infilename);
 my $count = 0;
@@ -13,9 +29,7 @@ while($line = <INFILE>)
 }
 close INFILE;
 
-my $png = "out.png";
-
-my $imgWidth = 32;
+#imgWidth already declared
 my $imgHeight = $count;
 
 my $surf = Cairo::ImageSurface->create ('rgb24', $imgWidth, $imgHeight);
@@ -28,10 +42,12 @@ $cr->fill;
 
 $cr->set_source_rgb (0, 0, 0);
 
+#Main loop: processes one number per iteration
 for($i = 0;$i < $count;$i++)
-{	
+{
 	$d = $data[$i];
-	for($j = 0; $j < 32;$j++)
+	#Each iteration processes one bit in => draws one line
+	for($j = 0; $j < $imgWidth;$j++)
 	{
 		$cr->new_path();
 		$cr->rectangle($j,$i,1,1) if $d & (2**$j);
@@ -40,4 +56,4 @@ for($i = 0;$i < $count;$i++)
 }
 
 $cr->show_page;
-$surf->write_to_png ($png);
+$surf->write_to_png ($outfilename);
