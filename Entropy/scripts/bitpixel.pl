388f7c6ed81fbc62348a205c1ffbfbc70106d2dc
Uli Koehler <ulikoehler@online.de>
2008-12-28 19:01:19 +0100

    Entropy: bitpixel.pl produces good results now.

--- Entropy/scripts/bitpixel.pl
+++ Entropy/scripts/bitpixel.pl
@@ -15,23 +15,24 @@ close INFILE;
 
 my $png = "out.png";
 
-my $surf = Cairo::ImageSurface->create ('rgb24', 32, $count*2);
+my $surf = Cairo::ImageSurface->create ('rgb24', 32, $count);
 my $cr = Cairo::Context->create ($surf);
 
 #Fill
-$cr->rectangle (0, 0, 32, $count);
+#$cr->rectangle (0, 0, 32*2, $count);
+#$cr->set_source_rgb (1, 1, 1);
+#$cr->fill;
+
 $cr->set_source_rgb (1, 1, 1);
-$cr->fill;
 
-$cr->set_source_rgb (0, 0, 0);
-
-for($i = 0;$i < length(@data);$i++)
+for($i = 0;$i < $count;$i++)
 {	
 	$d = $data[$i];
+	print "for i=".$i." d=".$d."\n";
 	for($j = 0; $j < 32;$j++)
 	{
 		$cr->new_path();
-		$cr->rectangle($j*2,$i*2,1,1) if ($d & (2**$i));
+		$cr->rectangle($j,$i,1,1) if $d & (2**$j);
 		$cr->stroke();
 	}
 }
