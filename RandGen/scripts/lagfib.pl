96517d65c4b8df274e96ba5c225872ac8b0ec7e0
Uli Koehler <ulikoehler@online.de>
2008-12-28 19:57:57 +0100

    RandGen: lagfib.pl: Added command line parameter defaults.

--- RandGen/scripts/lagfib.pl
+++ RandGen/scripts/lagfib.pl
@@ -1,21 +1,21 @@
 #!/usr/bin/perl -w
 #This script implements the lagged fibonacci generator
 #y_i = (y{i-A}+y{i-B}) mod m
-#Syntax is: lagfib.pl outfile n m A B [seed list]
+#Syntax is: lagfib.pl [outfile] [n] [m] [A] [B] [seed list]
 #Where:
-#out = The output filename
-#n = The number of numbers to generate
-#m = The modulus (as above)
-#A,B as above
-#[seed list] = A comma-separated list of seed values, at least A and at least B elements
+#[outfile] = The output filename. Defaults to "rand.txt"
+#[n] = The number of numbers to generate. Defaults to 100000
+#[m] = The modulus (as above). Defaults to 1e9
+#[A],[B] as above. Default to A=1;B=2
+#[seed list] = A comma-separated list of seed values, at least A and at least B elements. Defaults to "1,2"
 
 #Parse the command line
-my $ofname = shift;
-my $n = shift;
-my $m = shift;
-my $a = shift;
-my $b = shift;
-my $sraw = shift;
+my $ofname = shift || "rand.txt";
+my $n = shift || 100000;
+my $m = shift || 1000000000;
+my $a = shift || 1;
+my $b = shift || 2;
+my $sraw = shift || "1,2";
 my @s = split(/,/,$sraw);
 
 #Open the output file
