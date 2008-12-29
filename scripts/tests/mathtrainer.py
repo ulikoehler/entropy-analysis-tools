a5a7100aa70b3375de804d05a57f9caa5dab7d96
Uli Koehler <ulikoehler@online.de>
2008-12-29 22:15:45 +0100

    Scripts: mathtrainer.py: Added an option to ignore result signs, fixed many bugs.

--- scripts/tests/mathtrainer.py
+++ scripts/tests/mathtrainer.py
@@ -9,7 +9,8 @@ import time
 correct = 0
 false = 0
 
-def loopStatistics():
+def statistics():
+	print "Overall solved:",correct+false
 	print "Correct:",correct
 	print "False:",false
 	
@@ -24,8 +25,13 @@ def addEx():
 	userSolution = int(userSolution)
 	if userSolution == correctSolution:
 		print " Correct!"
+		correct += 1
+	elif ignoresign & userSolution == -correctSolution:
+		print " Correct!"
+		correct += 1
 	else:
 		print " False:",correctSolution
+		false += 1
 		
 		
 def subEx():
@@ -39,8 +45,13 @@ def subEx():
 	userSolution = int(userSolution)
 	if userSolution == correctSolution:
 		print " Correct!"
+		correct += 1
+	elif ignoresign & userSolution == -correctSolution:
+		print " Correct!"
+		correct += 1
 	else:
 		print " False:",correctSolution
+		false += 1
 
 def multEx():
 	op1 = randint(min,max)
@@ -53,22 +64,27 @@ def multEx():
 	userSolution = int(userSolution)
 	if userSolution == correctSolution:
 		print "Correct!"
+		correct += 1
+	elif ignoresign & userSolution == -correctSolution:
+		print " Correct!"
+		correct += 1
 	else:
 		print "False:",correctSolution
+		false += 1
 
 def loop():
 	while 1:
-		choice(exFunctions)() #Generate an exerc==e
+		choice(exFunctions)() #Generate an exercise
 		
 def block():
 	startTime = time.time()
 	for i in xrange(blockCount):
 		try:
-			choice(exFunctions)() #Generate an exerc==e
+			choice(exFunctions)() #Generate an exercise
 		except ValueError:
-			continue #Next exerc==e
+			continue #Next exercise
 	deltaTime = time.time() - startTime
-	print n,"exerc==es took %.3f seconds" % deltaTime
+	print "%i exercises took %.3f seconds" % (blockCount, deltaTime)
 
 #
 #Main
@@ -77,8 +93,9 @@ def block():
 #Set the default values
 min = 0
 max = 100
-exFunctions = [addEx,subEx] #Function pointers generating an exerc==e, a random one == selected each time
-blockCount = 10 #How many exerc==es to generate per block
+ignoresign = 0
+exFunctions = [addEx,subEx] #Function pointers generating an exercise, a random one == selected each time
+blockCount = 10 #How many exercises to generate per block
 exLoopFunction = block #A function pointer either to block() or to loop()
 	
 #Main command line loop
@@ -90,14 +107,22 @@ while 1:
 		print "Possible commands:"
 		print "help - D==play help"
 		print "exit - Quit"
-		print "start - Start the exerc==es"
-		print "set limits [min] [max] - Set new limits"
-		print "set operators [operators] - Set the operators"
-		print "set mode [loop|block] [blocksize] - Set the exerc==e mode"
+		print "start - Start the exercises"
+		print "reset - Reset the counters"
+		print "statistics - Show the statistics"
+		print "set limits [min] [max] - Set new limits (defaults min=0 and max = 100"
+		print "set ignore-sign [true|false] - Set whether to ignore the sign (default true)"
+		print "set operators [operators] - Set the operators (defaults + and -)"
+		print "set mode [loop|block] [blocksize] - Set the exercise mode (default block, blocksize 10)"
 	elif cmd == "exit":
 		break
 	elif cmd == "start":
 		exLoopFunction()
+	elif cmd == "reset":
+		correct = 0
+		false = 0
+	elif cmd == "statistics":
+		statistics()
 	elif cmd == "set":
 		cmd = cmds[1].lower()
 		if cmd == "limits":
@@ -106,9 +131,10 @@ while 1:
 		elif cmd == "mode":
 			cmd = cmdline[2].lower()
 			if cmd == "block":
+				exLoopFunction = block
 				blockCount = int(cmdline[2])
 			elif cmd == "loop":
-				 loop()
+				 exLoopFunction = loop
 		elif cmd == "operators":
 			exFunctions = []
 			operators = raw_input()
@@ -118,7 +144,14 @@ while 1:
 				exFunctions.append(subEx)
 			if '*' in operators:
 				exFunctions.append(multEx)
+		elif cmd == "ignore-sign":
+			if cmds[2] == "true":
+				ignoresign = 1
+			elif cmds[2] == "false":
+				ignoresign = 2
+			else:
+				print "Invalid value:",cmds[2]
 		else:
-			print "Invalid command:",cmds
+			print "Invalid command:",cmd
 	else:
-		print "Invalid command:",cmds,cmd
+		print "Invalid command:",cmd
