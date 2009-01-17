353249cefaf5afda700056a4a36e9fb6d65435d8
Uli Koehler <ulikoehler@online.de>
2009-01-17 15:30:51 +0100

    Added separate directory for all cryptography tools (Crypto); moved EasyBotan to Crypto/

---
+++ Crypto/EasyBotan/globals.hpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   globals.hpp
+ * Author: uli
+ *
+ * Created on 9. Januar 2009, 22:35
+ */
+
+#ifndef _GLOBALS_HPP
+#define	_GLOBALS_HPP
+
+#include <fstream> //Include iostream and string
+#include <botan/botan.h>
+
+using namespace Botan;
+
+#include <md.hpp>
+
+#endif	/* _GLOBALS_HPP */
+
---
+++ Crypto/EasyBotan/nbproject/Makefile-Debug.mk
@@ -0,0 +1,67 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=gfortran
+
+# Macros
+PLATFORM=GNU-Linux-x86
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=build/Debug/${PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/main.o
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	${MAKE}  -f nbproject/Makefile-Debug.mk dist/Debug/${PLATFORM}/easybotan
+
+dist/Debug/${PLATFORM}/easybotan: ${OBJECTFILES}
+	${MKDIR} -p dist/Debug/${PLATFORM}
+	${LINK.cc} -lbotan -lboost_program_options-gcc43-mt -o dist/Debug/${PLATFORM}/easybotan ${OBJECTFILES} ${LDLIBSOPTIONS} 
+
+${OBJECTDIR}/main.o: main.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	$(COMPILE.cc) -g -o ${OBJECTDIR}/main.o main.cpp
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf:
+	${RM} -r build/Debug
+	${RM} dist/Debug/${PLATFORM}/easybotan
+
+# Subprojects
+.clean-subprojects:
